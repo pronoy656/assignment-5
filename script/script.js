ff70c27bc4eb1d866b992db7ab48d50e135cc934
pronoy656 <pronoypaul56@gmail.com>
2024-02-18 17:02:49 +0600

    js section add

---
+++ script/script.js
@@ -0,0 +1,55 @@
+const allBtn = document.getElementsByClassName('btn-click');
+let seat = 0;
+let seatLeft = 40;
+for(const btn of allBtn){
+
+    btn.addEventListener('click',function (e){
+        seat = seat + 1;
+        seatLeft = seatLeft - 1;
+// console.log(e.target)
+e.target.classList.add('bg-green-400')
+       
+
+        const seatName = e.target.innerText;
+        const seatClass = document.getElementById('class').innerText;
+        const ticketPrice = document.getElementById('ticket-price').innerText;
+
+// ---------------------------------place seat name class and mrp-------------------
+         const container = document.getElementById('selected-container');
+
+         const li = document.createElement('li')
+
+         const p = document.createElement('p');
+         p.innerText = seatName;
+         const p1 = document.createElement('p');
+         p1.innerText = seatClass;
+         const p2 = document.createElement('p');
+         p2.innerText = ticketPrice;
+
+         li.appendChild(p);
+         li.appendChild(p1);
+         li.appendChild(p2);
+
+         container.appendChild(li);
+
+        //  ----------------------total cost and grand total cost ---------------------
+
+         const totalCost = document.getElementById('total-cost').innerText;
+         const convertTotalCost = parseInt(totalCost);
+         document.getElementById('total-cost').innerText =  convertTotalCost + parseInt(ticketPrice ) ;
+          
+         const grandTotal = document.getElementById('grand-total').innerText;
+         const convertGrandTOtal = parseInt(grandTotal);
+         document.getElementById('grand-total').innerText = convertGrandTOtal + parseInt(ticketPrice );
+
+         
+        if(seat > 4){
+              return alert('YOU CAN"T CHOOSE MORE THEN 4');
+        }
+
+
+        document.getElementById('seat-number').innerText = seat;
+        document.getElementById('seat-left').innerText = seatLeft;
+    })
+
+}
